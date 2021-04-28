855832ba863166f1ed97467d0caf126a8bac9887
Neo Chan <netkiller@msn.com>
2021-04-28 21:32:15 +0800

    update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as file:
 
 setup(
 	name='netkiller-kindle',
-	version='0.0.2',
+	version='0.0.3',
 	author='Neo Chen',
 	author_email='netkiller@msn.com',
 	description="Send ebook to kindle device",
